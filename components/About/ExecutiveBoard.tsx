8f280f35a1c69847a8e5d20932a0bd2ff674fbb2
magicalsoup <amagicalsoup@gmail.com>
2022-10-27 20:17:26 -0400

    fix executive board

--- components/About/ExecutiveBoard.tsx
+++ components/About/ExecutiveBoard.tsx
@@ -2,8 +2,8 @@ import ExecutiveCard from "./ExecutiveCard";
 
 export default function ExecBoard({ ExecData }) {
   return (
-    <div className="flex justify-around py-24">
-      <div className="flex flex-wrap gap-x-4 gap-y-4 md:gap-x-16 md:gap-y-16">
+    <div className="flex justify-center py-24">
+      <div className="flex flex-wrap justify-center gap-x-4 gap-y-4 xl:gap-x-16 md:gap-y-16">
         {ExecData.map((exec, index) => (
           <ExecutiveCard executive={exec} key={index} />
         ))}
