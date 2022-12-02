931d1f896942d85472c2112ce1e94465a15042af
Fiona Cai <69484076+fiona-cai@users.noreply.github.com>
2022-12-01 23:54:51 -0500

    Update ExecutiveData.tsx

--- data/ExecutiveData.tsx
+++ data/ExecutiveData.tsx
@@ -1,41 +1,53 @@
 /*
 
-TODO: Add executives data:
-Kevin Tang - General
-Aron Ravikumar - General
-Owen Guo - General
-Jaden Zhang - Competitive
-Alex Xu - Competitive
-Karen Lin - PR officer
-Mr Smintinch - Supervisor
-
 */
 
 export const EXECUTIVE_DATA = [
     {
-        name: "Lakshy-Gupta",
-        role: "Executive",
+        name: "Fiona Cai",
+        role: "General Executive",
         photo: "/images/executives/Lakshy-Gupta.JPG",
         
     },
     {
-        name: "Jaden Zhang",
+        name: "Owen Guo",
         role: "General Executive",
-        photo: "/images/executives/Jaden-Zhang.JPG",
-    },
-    {
-        name: "Yuming He",
-        role: "Competitive Executive",
         photo: "/images/executives/Yuming-He.JPG",
     },   
     {
-        name: "Fiona Cai",
-        role: "PR Officer",
+        name: "Applearon",
+        role: "General Executive",
+        photo: "/images/executives/Gloria-Li.JPG"
+    },
+    {
+        name: "Kevin Tang",
+        role: "General Executive",
         photo: "/images/executives/Fiona-Cai.JPG"
     },
     {
+        name: "Hidden Cactus",
+        role: "General Executive",
+        photo: "/images/executives/Fiona-Cai.JPG"
+    },
+    {
+        name: "Alex Xu",
+        role: "Competitive Executive",
+        photo: "/images/executives/Jaden-Zhang.JPG",
+    },
+    {
+        name: "Jaden Zhang",
+        role: "Competitive Executive",
+        photo: "/images/executives/Jaden-Zhang.JPG",
+    },
+    {
         name: "Gloria Li",
-        role: "PR Officer",
-        photo: "/images/executives/Gloria-Li.JPG"
-    }
+        role: "Competitive Executive",
+        photo: "/images/executives/Jaden-Zhang.JPG",
+    },
+    {
+        name: "Karen Lin",
+        role: "Competitive Executive",
+        photo: "/images/executives/Jaden-Zhang.JPG",
+    },
+        
 ]
